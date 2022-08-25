0e2316efa6a20c30d8fb69f78358f20fd7fe0449
FelipeReggiane <felipe.reggiane@rethink.dev>
2022-08-25 09:15:33 -0300

    feature/Evaluation creating evaluate table

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from "express";
+import EvaluateController from "./controller/EvaluateController";
 import NoteController from "./controller/NoteController";
 import UserController from "./controller/UserController";
 
@@ -13,5 +14,10 @@ router.get("/note/:email", NoteController.getNotesByUser);
 router.post("/note", NoteController.create);
 router.post("/note/:id", NoteController.update);
 router.delete("/note/:id", NoteController.remove);
+
+router.get("/evaluate/:month", EvaluateController.getEvaluates);
+router.post("/evaluate", EvaluateController.create);
+router.post("/evaluate/:id", EvaluateController.update);
+router.delete("/evaluate/:id", EvaluateController.remove);
 
 export { router };
